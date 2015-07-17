e35d28264895018af2ec0ba18dd9e0be108569f5
Erik Arvidsson <erik.arvidsson@gmail.com>
2015-07-17 09:03:06 -0700

    Add test file and travis yaml

---
+++ test_test.go
@@ -0,0 +1,17 @@
+package foo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPass(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(1, 1)
+}
+
+// func TestFail(t *testing.T) {
+// 	assert := assert.New(t)
+// 	assert.NotEqual(1, 1)
+// }
